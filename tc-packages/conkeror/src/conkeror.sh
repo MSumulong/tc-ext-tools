d3b38a4d38e5698fbec8b9fb0d3a1dff11b79346
arslanserc <arslanserc@78d6d9dc-b644-c8ff-05df-608b05a54344>
2011-02-20 18:16:09 +0000

    - Updated conkeror package

--- tc-packages/conkeror/src/conkeror.sh
+++ tc-packages/conkeror/src/conkeror.sh
@@ -3,7 +3,5 @@
 conkeror=/usr/local/conkeror
 xulrunner=/usr/local/bin/xulrunner
 
-if [ -x "$xulrunner" ]; then
-     exec "$xulrunner" "$conkeror"/application.ini "$@"
-fi
+exec "$xulrunner" "$conkeror"/application.ini "$@"
 
