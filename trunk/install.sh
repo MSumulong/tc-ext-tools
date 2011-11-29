a6fdc12e3155df6614ad7e3093e6ebd916f4578d
arslanserc@gmail.com <arslanserc@gmail.com@78d6d9dc-b644-c8ff-05df-608b05a54344>
2011-11-29 08:00:03 +0000

    - Updated minitube and dropbox

--- trunk/install.sh
+++ trunk/install.sh
@@ -47,7 +47,7 @@ DATADIR="$DESTDIR/share"
 install -m 755 tools/* "$BINDIR"
 
 install -D -m 644 common/config "$SYSCONFDIR/tet.conf"
-install -D -m 755 common/tet-functions "$SYSCONFDIR/init.d/tet-functions"
+install -D -m 755 tools/tet-functions "$SYSCONFDIR/init.d/tet-functions"
 install -D -m 644 common/build "$DATADIR/tet/build.sample"
 install -D -m 755 common/functions "$DATADIR/tet/functions.sh"
 
