a2514fd12db1095a8398fbf4f7acc7300c0bce1d
arslanserc <arslanserc@78d6d9dc-b644-c8ff-05df-608b05a54344>
2012-07-28 10:16:24 +0000

    added murmur

---
+++ packages/murmur/src/murmur.conf.d
@@ -0,0 +1,2 @@
+# Use /usr/local/etc/murmur.ini as configuration file
+PARAMS="-ini /usr/local/etc/murmur.ini"
---
+++ packages/murmur/src/murmur.init.d
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+MURMURD=/usr/local/sbin/murmurd
+CONF=/usr/local/etc/conf.d/murmur
+
+. /etc/init.d/tc-functions
+
+[ -f $CONF ] && . $CONF
+
+PID=$(pidof -o %PPID $MURMURD)
+case "$1" in
+  start)
+    echo -e "\nStarting murmur ... \c"
+
+    [ -d /var/run/murmur ] || mkdir -p /var/run/murmur
+    chown murmur:murmur /var/run/murmur
+
+    [ -z "$PID" ] && $MURMURD $PARAMS
+    if [ $? -gt 0 ]; then
+      echo -e "failed!\n"
+    else
+      echo -e "ok!\n"
+    fi
+    ;;
+  stop)
+    echo -e "\nStopping murmur ... \c"
+    [ ! -z "$PID" ]  && kill $PID &> /dev/null
+    if [ $? -gt 0 ]; then
+      echo -e "failed!\n"
+    else
+      echo -e "ok!\n"
+    fi
+    ;;
+  restart)
+    $0 stop
+    sleep 1
+    $0 start
+    ;;
+  status)
+    if [ -z "$PID" ]; then
+      echo -e "\nmurmur is not running\n"
+      exit 1
+    else
+      echo -e "\nmurmur is running\n"
+      exit 0
+    fi
+    ;;
+  *)
+    echo "usage: $0 {start|stop|restart|status}"
+esac
+exit 0
